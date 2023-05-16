2e2845fead99f69a805bde2d9da423de87716503
takie-dela-tovarisch <polinasot@gmail.com>
2023-05-16 17:10:37 +0300

    Added yandex analytics, refactored script, added some styling

--- js/anima.js
+++ js/anima.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  // $(".join").hover(function () {
+  //   $(".animated_lights").animate({ opacity: 1 }, { duration: 1000 });
+  // });
+  $(".join").on("mouseenter", function () {
+    $(".animated_lights").animate({ opacity: 1 }, { duration: 1000 });
+  });
+  $(".join").on("mouseleave", function () {
+    $(".animated_lights").animate({ opacity: 0 }, { duration: 1000 });
+  });
 
-$('.join').hover(
-  function(){ $("body").toggleClass('lights') },
-)
-
+  $(".scroll_to_bottom").click(function () {
+    $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+  });
 });
