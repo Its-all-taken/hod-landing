6e954653c77f11a424476f2983f23363dcb129e2
takie-dela-tovarisch <polinasot@gmail.com>
2023-05-16 18:28:27 +0300

    Changed OG descr

--- js/anima.js
+++ js/anima.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
-  // $(".join").hover(function () {
-  //   $(".animated_lights").animate({ opacity: 1 }, { duration: 1000 });
-  // });
   $(".join").on("mouseenter", function () {
     $(".animated_lights").animate({ opacity: 1 }, { duration: 1000 });
   });
   $(".join").on("mouseleave", function () {
     $(".animated_lights").animate({ opacity: 0 }, { duration: 1000 });
   });
-
   $(".scroll_to_bottom").click(function () {
     $("html, body").animate({ scrollTop: $(document).height() }, 1000);
   });
